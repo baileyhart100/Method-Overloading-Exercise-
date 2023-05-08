0d0f5b12fe39b333d8962a5ee561d4dc991f53ff
Bailey Hart <baileyhart100@gmail.com>
2023-05-08 16:25:40 -0400

    initial commit

--- MethodOverloading/Program.cs
+++ MethodOverloading/Program.cs
@@ -2,9 +2,45 @@
 {
     public class Program
     {
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine(Add(0, 1, true));
+
+
+            Console.WriteLine(Add(5, 7, true));
+
+
+            Console.WriteLine(Add(-6, 3, true));
+        }
+
+
+        public static int Add(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+
+        public static decimal Add(decimal decimal1, decimal decimal2)
+        {
+            return decimal1 + decimal2;
+        }
+
+        public static string Add(int num1, int num2, bool WithDollars)
+        {
+            if (WithDollars && (num1 + num2 != 1))
+            {
+                return $"{num1 + num2} dollars";
+            }
+            else if (WithDollars && (num1 + num2) == 1)
+            {
+                return ($"{num1 + num2} dollar");
+            }
+            else
+            {
+                return $"{num1 + num2}";
+            }
+
         }
     }
 }
+
